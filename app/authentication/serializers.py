dbd3fb56e02f2d2c08e86c80f107b0e8b6be7ec1
davinoroes <davinoroes1918@gmail.com>
2026-05-27 12:35:00 -0300

    App authentication, com serializers e views

---
+++ app/authentication/serializers.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class RegisterUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True,min_length=6)
+
+    class Meta:
+        model = User
+        fields = ('id','username','email','password')
+
+    def create(self,validated_data):
+        user = User.objects.create_user(
+                username=validated_data['username'],
+                password=validated_data['password'],
+            )
+        return user
+        
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id','username','email')
+
+
+
+
+
