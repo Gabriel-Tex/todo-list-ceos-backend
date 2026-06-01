ca0a98e9d1551fce236d3cffae082530f956bb34
davinoroes <davinoroes1918@gmail.com>
2026-06-01 11:09:52 -0300

    Retirada da importação do authtoken

--- app/authentication/views.py
+++ app/authentication/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import authenticate
 from rest_framework import generics, status
-from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
  
