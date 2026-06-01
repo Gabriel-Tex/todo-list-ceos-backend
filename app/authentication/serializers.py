884468ff947be2cf3a0ab793ae5fe1f202e0c22c
ArthurFernandes06 <artrdgs@gmail.com>
2026-05-31 23:17:32 -0300

    Configurando o CORS e consertando bug de serializer

--- app/authentication/serializers.py
+++ app/authentication/serializers.py
@@ -12,7 +12,9 @@ class RegisterUserSerializer(serializers.ModelSerializer):
     def create(self,validated_data):
         user = User.objects.create_user(
                 username=validated_data['username'],
+                email=validated_data['email'],
                 password=validated_data['password'],
+                
             )
         return user
         
