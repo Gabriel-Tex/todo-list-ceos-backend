551dfaac0f93e45c0721bbd8b24ad9a2de488c4e
davinoroes <davinoroes1918@gmail.com>
2026-06-01 11:10:25 -0300

    Criação do model de tarefas da todolist

--- app/tasks/models.py
+++ app/tasks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+PRIORITY_CHOICES = [
+    ('low','BAIXA'),
+    ('medium', 'MEDIA'),
+    ('high', 'ALTA'),
+]
+
+class Tasks(models.Model):
+    tittle = models.CharField(max_length=50)
+    priority = models.CharField(max_length=10,choices=PRIORITY_CHOICES)
+    owner = models.ForeignKey(User,on_delete=models.CASCADE,related_name='tasks')
+    final_date = models.DateField(blank=True,null=True)
+
