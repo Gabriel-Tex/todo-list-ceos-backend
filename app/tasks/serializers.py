b3458c4d3f24652cbeae025e98c1df0ba12848e2
Gabriel Teixeira <gabrieldamasqueiroz@gmail.com>
2026-06-04 20:27:12 -0300

    feat: adicionando filtros de busca e campos em Task.

--- app/tasks/serializers.py
+++ app/tasks/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
-from .models import Tasks
+from .models import Task, PRIORITY_CHOICES, STATUS_CHOICES
 
 
 class TaskSerializer(serializers.ModelSerializer):
     owner = serializers.PrimaryKeyRelatedField(read_only=True) 
+    created_at = serializers.DateTimeField(read_only=True)
 
     class Meta:
-        model = Tasks
-        fields = ['id', 'title', 'priority', 'owner', 'final_date']
+        model = Task
+        fields = ['id', 'title', 'description', 'priority', 'status', 'owner', 'final_date', 'created_at',]
--- app/tasks/urls.py
+++ app/tasks/urls.py
@@ -3,6 +3,6 @@ from tasks.views import TaskListCreateView, TaskRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('tasks/',TaskListCreateView.as_view(),name='task-list-create'),
-    path('tasks/<int:pk>',TaskRetrieveUpdateDestroyView.as_view(),name='delete-update-task'),
+    path('tasks/<int:pk>/',TaskRetrieveUpdateDestroyView.as_view(),name='delete-update-task'),
 
 ]
